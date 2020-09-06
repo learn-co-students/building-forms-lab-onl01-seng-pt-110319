1be2c6a8ed7efbe7314f9594741a538a17c29028
Aida Abdikulova <aabdikul@Aidas-MacBook-Pro.local>
2020-09-06 00:44:01 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,13 @@
-export default function manageBand(state = {
-  bands: [],
-}, action) {
-  return state
+export default function manageBand(state = {bands: [],}, action) {
+  
+	switch (action.type) {
+		case 'ADD_BAND':
+		const newName = {name: action.payload}
+		return { ...state, bands: [...state.bands, newName] }
+
+		
+	default:
+		return state
+	}
 };
+
